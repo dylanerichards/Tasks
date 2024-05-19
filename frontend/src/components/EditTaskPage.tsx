be702d0aa98f286cd3a2245f472017476d02f0c8
Dylan E. Richards <dylanerichards@gmail.com>
2024-05-19 19:21:52 -0400

    Add handler to update description of task

--- frontend/src/components/EditTaskPage.tsx
+++ frontend/src/components/EditTaskPage.tsx
@@ -12,10 +12,12 @@ const EditTaskPage: React.FC = () => {
     const { data, loading } = useQuery(GET_TASK_BY_ID, { variables: { id: task_id } });
 
     const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
 
     useEffect(() => {
         if (data && data.task) {
           setTitle(task.title);
+          setDescription(task.description);
         }
       }, [data]);
 
@@ -24,6 +26,7 @@ const EditTaskPage: React.FC = () => {
     const task = data.task
 
     const handleTitleChange = (e: any) => setTitle(e.target.value);
+    const handleDescriptionChange = (e: any) => setDescription(e.target.value);
 
     return (
         <form className="flex w-full max-w-sm space-x-3">
@@ -34,7 +37,7 @@ const EditTaskPage: React.FC = () => {
                 <div className="grid max-w-xl grid-cols-2 gap-4 m-auto">
                     <div className="col-span-2 lg:col-span-1">
                         <div className=" relative ">
-                            <input value={title} onChange={handleTitleChange} type="text" id="contact-form-name" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Name" />
+                            <input value={title} onChange={handleTitleChange} type="text" id="contact-form-name" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Title" />
                         </div>
                     </div>
                     <div className="col-span-2 lg:col-span-1">
@@ -44,7 +47,7 @@ const EditTaskPage: React.FC = () => {
                     </div>
                     <div className="col-span-2">
                         <label className="text-gray-700" htmlFor="name">
-                            <textarea className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-gray-300 rounded-lg appearance-none focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" id="comment" placeholder="Enter your comment" name="comment" rows={5} cols={40}>
+                            <textarea value={description} onChange={handleDescriptionChange} className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-gray-300 rounded-lg appearance-none focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" id="comment" placeholder="description" name="description" rows={5} cols={40}>
                             </textarea>
                         </label>
                     </div>
