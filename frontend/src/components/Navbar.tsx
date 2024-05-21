a3fe0b6b9ead6a2fbd289a1c88769ac3294ef72f
Dylan E. Richards <dylanerichards@gmail.com>
2024-05-21 03:30:27 -0400

    Update New Task + button in Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -10,12 +10,9 @@ const Navbar: React.FC = () => {
           <Link to="/">Tasks</Link>
         </div>
         <Link to="/tasks/new" className="text-white hover:text-gray-300 ml-3">
-          <div className="md:flex space-x-4 text-white">
-            New Task
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-            </svg>
-          </div>
+        <button className="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded">
+            New Task +
+    </button>
         </Link>
       </div>
     </nav>
