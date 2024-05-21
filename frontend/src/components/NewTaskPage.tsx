7379c637d4856ca1c9ca95a96a8489f97e78a46f
Dylan E. Richards <dylanerichards@gmail.com>
2024-05-21 03:19:06 -0400

    Lighten submit button when validations are not met

--- frontend/src/components/NewTaskPage.tsx
+++ frontend/src/components/NewTaskPage.tsx
@@ -74,9 +74,17 @@ const EditTaskPage: React.FC = () => {
             </div>
 
             <div className="col-span-2 text-right">
-              <button disabled={title.length == 0 || date == null} type="submit" className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
-                Submit
-              </button>
+            <button
+  disabled={title.length === 0 || date == null}
+  type="submit"
+  className={`py-2 px-4 ${
+    title.length === 0 || date == null
+      ? 'bg-indigo-400' // Lighter shade of purple when disabled
+      : 'bg-indigo-600 hover:bg-indigo-700'
+  } focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg `}
+>
+  Submit
+</button>
             </div>
           </div>
         </div>
